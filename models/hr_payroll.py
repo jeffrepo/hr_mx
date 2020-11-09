718cbad37d845c17740476b10935911b1eecc3cc
jefferson <jeffer.leo93@gmail.com>
2020-11-09 09:20:47 -0600

    Entry

--- models/hr_payroll.py
+++ models/hr_payroll.py
@@ -8,9 +8,9 @@ class HrPayslip(models.Model):
     def compute_sheet(self):
         for nomina in self:
             entradas = False
-            if contrato_id.structure_type_id and contrato_id.structure_type_id.default_struct_id:
-                if contrato_id.structure_type_id.default_struct_id.input_line_type_ids:
-                    entradas = [entrada for entrada in contrato_id.structure_type_id.default_struct_id.input_line_type_ids]
+            if nomina.employee_id.contract_id.structure_type_id and  nomina.employee_id.contract_id.structure_type_id.default_struct_id:
+                if  nomina.employee_id.contract_id.structure_type_id.default_struct_id.input_line_type_ids:
+                    entradas = [entrada for entrada in  nomina.employee_id.contract_id.structure_type_id.default_struct_id.input_line_type_ids]
             if entradas:
                 for entrada in entradas:
                     entrada_id = self.env['hr.payslip.input'].create({'payslip_id': nomina.id,'input_type_id': entrada.id})
